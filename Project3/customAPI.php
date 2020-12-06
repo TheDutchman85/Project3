5bb83a19a7d12ed7981db01d9b294f1650ec989c
Lucas Veldman <55513822+TheDutchman85@users.noreply.github.com>
2020-12-06 02:42:50 -0500

    Add files via upload

---
+++ Project3/customAPI.php
@@ -0,0 +1,455 @@
+<?php
+
+$locations = [
+  [
+    "name"=>"Great Wall of China",
+    "lat"=>"40.4319",
+    "lon"=>"116.5704",
+  ],
+  [
+    "name"=>"Christ the Redeemer Statue",
+    "lat"=>"-22.9519",
+    "lon"=>"-43.2105",
+  ],
+  [
+    "name"=>"Machu Picchu",
+    "lat"=>"-13.1631",
+    "lon"=>"-72.5450",
+  ],
+  [
+    "name"=>"Chichen Itza",
+    "lat"=>"20.6843",
+    "lon"=>"-88.5678",
+  ],
+  [
+    "name"=>"The Roman Colosseum",
+    "lat"=>"41.8902",
+    "lon"=>"12.4922",
+  ],
+  [
+    "name"=>"Taj Mahal",
+    "lat"=>"27.1751",
+    "lon"=>"78.0421",
+  ],
+  [
+    "name"=>"Petra",
+    "lat"=>"30.3285",
+    "lon"=>"35.4444",
+  ],
+  [
+    "name"=>"Stonehenge ",
+    "lat"=>"51.1789",
+    "lon"=>"-1.8262",
+  ],
+  [
+    "name"=>"Leaning Tower of Pisa",
+    "lat"=>"43.7230",
+    "lon"=>"10.3966",
+  ],
+  [
+    "name"=>"Sydney Opera House ",
+    "lat"=>"-33.8568",
+    "lon"=>"151.2153",
+  ],//10
+  [
+    "name"=>"Eiffel Tower ",
+    "lat"=>"48.8584",
+    "lon"=>"2.2945",
+  ],
+  [
+    "name"=>"Statue of Liberty ",
+    "lat"=>"40.6892",
+    "lon"=>"-74.0445",
+  ],
+  [
+    "name"=>"Angkor Wat",
+    "lat"=>"13.4125",
+    "lon"=>"103.8670",
+  ],
+  [
+    "name"=>"Mont Saint-Michel ",
+    "lat"=>"48.6361",
+    "lon"=>"-1.5115",
+  ],
+  [
+    "name"=>"St. Basil's Cathedral",
+    "lat"=>"55.7525",
+    "lon"=>"37.6231",
+  ],
+  [
+    "name"=>"The Acropolis",
+    "lat"=>"37.9715",
+    "lon"=>"23.7257"
+  ],
+  [
+    "name"=>"Easter Island",
+    "lat"=>"-27.1127",
+    "lon"=>"-109.3497"
+  ],
+  [
+    "name"=>"Neuschwanstein Castle",
+    "lat"=>"47.5576",
+    "lon"=>"10.7498"
+  ],
+  [
+    "name"=>"The Great Pyramid of Giza",
+    "lat"=>"29.9792",
+    "lon"=>"31.1342"
+  ],
+  [
+    "name"=>"Victoria Falls",
+    "lat"=>"-17.9316",
+    "lon"=>"25.8302"
+  ],//20
+  [
+    "name"=>"The Western Wall",
+    "lat"=>"31.7767",
+    "lon"=>"35.2345"
+  ],
+  [
+    "name"=>"Giant's Causeway",
+    "lat"=>"55.2408",
+    "lon"=>"-6.5116"
+  ],
+  [
+    "name"=>"Buckingham Palace",
+    "lat"=>"51.5014",
+    "lon"=>"-0.1419"
+  ],
+  [
+    "name"=>"La Sagrada Familia",
+    "lat"=>"41.4036",
+    "lon"=>"2.1744"
+  ],
+  [
+    "name"=>"Blue Mosque",
+    "lat"=>"41.0054",
+    "lon"=>"28.9768"
+  ],
+  [
+    "name"=>"Blue Domes of Oia",
+    "lat"=>"36.3932",
+    "lon"=>"25.4615"
+  ],
+  [
+    "name"=>"Ha Long Bay",
+    "lat"=>"20.9101",
+    "lon"=>"107.1839"
+  ],
+  [
+    "name"=>"Vestrahorn",
+    "lat"=>"64.2733",
+    "lon"=>"-15.0086"
+  ],
+  [
+    "name"=>"The Pitons",
+    "lat"=>"13.8033",
+    "lon"=>"-61.0670"
+  ],
+  [
+    "name"=>"Denali National Park",
+    "lat"=>"63.1148",
+    "lon"=>"-151.1926"
+  ],//30
+  [
+    "name"=>"Laamu Atoll",
+    "lat"=>"1.9431",
+    "lon"=>"73.4180"
+  ],
+  [
+    "name"=>"Great Barrier Reef",
+    "lat"=>"-18.2871",
+    "lon"=>"147.6992"
+  ],
+  [
+    "name"=>"James Bond Island",
+    "lat"=>"8.2745",
+    "lon"=>"98.5012"
+  ],
+  [
+    "name"=>"Skellig Michael",
+    "lat"=>"51.7707",
+    "lon"=>"-10.5405"
+  ],
+  [
+    "name"=>"Milford Sound",
+    "lat"=>"-44.6414",
+    "lon"=>"167.8974"
+  ],
+  [
+    "name"=>"Uluru-kata tjuta national park",
+    "lat"=>"-25.3438",
+    "lon"=>"131.0347"
+  ],
+  [
+    "name"=>"Mount Rainier National Park",
+    "lat"=>"46.8800",
+    "lon"=>"-121.7269"
+  ],
+  [
+    "name"=>"Whitehaven beach",
+    "lat"=>"-20.2824",
+    "lon"=>"149.0389"
+  ],
+  [
+    "name"=>"Plitvice Lakes",
+    "lat"=>"44.8654",
+    "lon"=>"15.5820"
+  ],
+  [
+    "name"=>"Grace bay beach",
+    "lat"=>"21.7980",
+    "lon"=>"-72.1761"
+  ],//40
+  [
+    "name"=>"Bisti Badlands/De-Na-Zin Wilderness",
+    "lat"=>"36.2921",
+    "lon"=>"-108.1298"
+  ],
+  [
+    "name"=>"Rangiroa Atoll",
+    "lat"=>"-15.1162",
+    "lon"=>"-147.6513"
+  ],
+  [
+    "name"=>"Pulpit rock",
+    "lat"=>"58.9864",
+    "lon"=>"6.1904"
+  ],
+  [
+    "name"=>"Grand Canyon",
+    "lat"=>"36.0544",
+    "lon"=>"-112.1401"
+  ],
+  [
+    "name"=>"Waimea Canyon",
+    "lat"=>"22.0715",
+    "lon"=>"-159.6617"
+  ],
+  [
+    "name"=>"Mount Pilatus",
+    "lat"=>"46.9795",
+    "lon"=>"8.2548"
+  ],
+  [
+    "name"=>"kirkjufell",
+    "lat"=>"64.9417",
+    "lon"=>"-23.3069"
+  ],
+  [
+    "name"=>"Redwood national park",
+    "lat"=>"41.2132",
+    "lon"=>"-124.0046"
+  ],
+  [
+    "name"=>"Serengeti national park",
+    "lat"=>"-2.3333",
+    "lon"=>"34.8333"
+  ],
+  [
+    "name"=>"Bora Bora",
+    "lat"=>"-16.5004",
+    "lon"=>"-151.7415"
+  ],//50
+  [
+    "name"=>"Niagara Falls",
+    "lat"=>"43.0896",
+    "lon"=>"-79.0849"
+  ],
+  [
+    "name"=>"Argentine Patagonia",
+    "lat"=>"-41.8101",
+    "lon"=>"-68.9063"
+  ],
+  [
+    "name"=>"Bali",
+    "lat"=>"-8.3405",
+    "lon"=>"115.0920"
+  ]
+];
+
+$cities = [
+  [
+    "name"=>"Marrakesh",
+    "lat"=>"31.6295",
+    "lon"=>"-7.9811"
+  ],
+  [
+    "name"=>"Dubrovnik",
+    "lat"=>"42.6507",
+    "lon"=>"18.0944"
+  ],
+  [
+    "name"=>"Cinque Terre",
+    "lat"=>"44.1461",
+    "lon"=>"9.6439"
+  ],
+  [
+    "name"=>"Copenhagen",
+    "lat"=>"55.6761",
+    "lon"=>"12.5683"
+  ],
+  [
+    "name"=>"Florence",
+    "lat"=>"43.7696",
+    "lon"=>"11.2558"
+  ],
+  [
+    "name"=>"Banff",
+    "lat"=>"51.1784",
+    "lon"=>"-115.5708"
+  ],
+  [
+    "name"=>"ölüdeniz",
+    "lat"=>"36.5704",
+    "lon"=>"29.1405"
+  ],
+  [
+    "name"=>"London",
+    "lat"=>"51.5074",
+    "lon"=>"-0.1278"
+  ],
+  [
+    "name"=>"Sydney",
+    "lat"=>"-33.8688",
+    "lon"=>"151.2093"
+  ],
+  [
+    "name"=>"Paris",
+    "lat"=>"48.8566",
+    "lon"=>"2.3522"
+  ],//10
+  [
+    "name"=>"Nice",
+    "lat"=>"43.7102",
+    "lon"=>"7.2620"
+  ],
+  [
+    "name"=>"Rome",
+    "lat"=>"41.9028",
+    "lon"=>"12.4964"
+  ],
+  [
+    "name"=>"Lisbon",
+    "lat"=>"38.7223",
+    "lon"=>"-9.1393"
+  ],
+  [
+    "name"=>"York",
+    "lat"=>"53.9600",
+    "lon"=>"-1.0873"
+  ],
+  [
+    "name"=>"Seattle",
+    "lat"=>"47.6062",
+    "lon"=>"-122.3321"
+  ],
+  [
+    "name"=>"Rio De Janeiro",
+    "lat"=>"-22.9068",
+    "lon"=>"-43.1729"
+  ],
+  [
+    "name"=>"Cairo",
+    "lat"=>"30.0444",
+    "lon"=>"31.2357"
+  ],
+  [
+    "name"=>"Athens",
+    "lat"=>"37.9838",
+    "lon"=>"23.7275"
+  ],
+  [
+    "name"=>"Venice",
+    "lat"=>"45.4408",
+    "lon"=>"12.3155"
+  ],
+  [
+    "name"=>"Amsterdam",
+    "lat"=>"52.3676",
+    "lon"=>"4.9041"
+  ],//20
+  [
+    "name"=>"Bangkok",
+    "lat"=>"13.7563",
+    "lon"=>"100.5018"
+  ],
+  [
+    "name"=>"Dubai",
+    "lat"=>"25.2048",
+    "lon"=>"55.2708"
+  ],
+  [
+    "name"=>"Istanbul",
+    "lat"=>"41.0082",
+    "lon"=>"28.9784"
+  ],
+  [
+    "name"=>"Antalya",
+    "lat"=>"36.8969",
+    "lon"=>"30.7133"
+  ],
+  [
+    "name"=>"Cape Town",
+    "lat"=>"-33.9249",
+    "lon"=>"18.4241"
+  ],
+  [
+    "name"=>"Berlin",
+    "lat"=>"52.5200",
+    "lon"=>"13.4050"
+  ],
+  [
+    "name"=>"Prague",
+    "lat"=>"50.0755",
+    "lon"=>"14.4378"
+  ],
+  [
+    "name"=>"Stockholm",
+    "lat"=>"59.3293",
+    "lon"=>"18.0686"
+  ],
+  [
+    "name"=>"Madrid",
+    "lat"=>"40.4168",
+    "lon"=>"-3.7038"
+  ],
+  [
+    "name"=>"Munich",
+    "lat"=>"48.1351",
+    "lon"=>"11.5820"
+  ],//30
+  [
+    "name"=>"Jerusalem",
+    "lat"=>"31.7683",
+    "lon"=>"35.2137"
+  ]
+];
+/*
+[
+    "name"=>"AAAAA",
+    "lat"=>"",
+    "lon"=>""
+  ],
+*/
+
+$type = "locations";
+if(array_key_exists('type', $_GET)){
+  $type = $_GET['type'];
+  if($type != "cities" && $type !="locations"){
+    $type = "locations";
+  }
+}
+
+if($type == "locations"){
+  $string = json_encode($locations);
+}else{
+  $string = json_encode($cities);
+}
+
+header('content-type:application/json');      // tell the requestor that this is JSON
+header("Access-Control-Allow-Origin: *");     // turn on CORS
+
+echo($string);
+
+?>
